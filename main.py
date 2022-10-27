530292c874f7feb5acdbeea53155b00816b09171
spcoughlin <56spc56@gmail.com>
2022-10-27 18:14:19 -0400

    better haar and added text

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def interperetCode(c1, c2, gray):
     for i in row1:
         x_val = c1[0] + int(i)
         y = c1[1]
-        print(x_val, y)
-        if np.any(gray[y, x_val] < 50):
+        if np.any(gray[y, x_val] < 128):
             bin_str += "1"
         else:
             bin_str += "0"
@@ -30,16 +29,13 @@ def interperetCode(c1, c2, gray):
     for i in row2:
         x_val = c1[0] + int(i)
         y = c2[1]
-        print(x_val, y)
-        if np.any(gray[y, x_val] < 50):
+        if np.any(gray[y, x_val] < 128):
             bin_str += "1"
         else:
             bin_str += "0"
         x_val = 0
         y = 0
 
-    print(bin_str)
-    cv2.imwrite("graywdots.png", gray)
     return bin_str
 
 
@@ -48,16 +44,18 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
+    cv2.resize(frame, (1280, 720))
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cats = catCascade.detectMultiScale(
         gray,
-        scaleFactor=1.15,
-        minNeighbors=5,
+        scaleFactor=1.01,
+        minNeighbors=4,
     )
 
     for (x, y, w, h) in cats:
-        cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
     if len(cats) >= 2:
         x, y, w, h = cats[0]
@@ -66,15 +64,14 @@ while True:
         x, y, w, h = cats[1]
         c2 = [int((2 * x + w)/2), int((2 * y + h)/2)]
 
-        if c1[0] < c2[0]: # makiing sure c1 is the one on the left
+        if c1[0] < c2[0]: # making sure c1 is the one on the left
             pass
         elif c1[0] > c2[0]:
             temp = c1
             c1 = c2
             c2 = temp
 
-        code = int(interperetCode(c1, c2, gray), 2)
-        break
+        cv2.putText(frame, f'Code #: {int(interperetCode(c1, c2, gray), 2)}', (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
 
     cv2.imshow('Video', frame)
 
@@ -83,7 +80,6 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-print(code)
 video_capture.release()
 cv2.destroyAllWindows()
 
