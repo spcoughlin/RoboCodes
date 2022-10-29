916051a7258fdeb41a9b6ea742f1f4b47c5e7afd
spcoughlin <56spc56@gmail.com>
2022-10-29 00:05:15 -0400

    moved some stuff around

--- making_txt_files.py
+++ making_txt_files.py
@@ -1,6 +1,9 @@
+import shutil
+
 members = 65
 lines = ["None\n", "None\n", "0"]
 for i in range(members):
     with open(f'hoursfile{i}.txt', 'w') as f:
         f.writelines(lines)
+        shutil.move(f"hoursfile{i}.txt", f"C:/HoursFiles/hoursfile{i}.txt")
         f.close()
